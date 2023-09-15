389362c0b1b2b9d5f1124b92c502b4ba3d56a444
ling-ling30 <126201256+ling-ling30@users.noreply.github.com>
2023-09-15 22:29:23 +0700

    middlewares

--- src/api/middlewares/verifyTokenMiddleware.js
+++ src/api/middlewares/verifyTokenMiddleware.js
@@ -1,6 +1,8 @@
 const { verifyToken } = require("../../lib/jwt");
+const prisma = require("../../lib/prisma");
+const CustomAPIError = require("./custom-error");
 
-const verifyTokenMiddleware = (req, res, next) => {
+const verifyTokenUser = (req, res, next) => {
   const token = req.headers.authorization;
   // console.log(req.headers.authorization);
   if (!token) {
@@ -9,7 +11,11 @@ const verifyTokenMiddleware = (req, res, next) => {
   // console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
   try {
     const decodedToken = verifyToken(token);
-    console.log(`after`);
+    const { id, username } = decodedToken;
+    const CheckUser = prisma.user.findUnique({ where: { id: id } });
+    if (!CheckUser) {
+      throw new CustomAPIError("Unauthorized", 401);
+    }
     req.user = decodedToken; // Store user information in request object for later use
     next(); // Move to the next middleware or route handler
   } catch (error) {
@@ -17,4 +23,24 @@ const verifyTokenMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = verifyTokenMiddleware;
+const verifyTokenAdmin = (req, res, next) => {
+  const token = req.headers.authorization;
+  // console.log(req.headers.authorization);
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  // console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+  try {
+    const decodedToken = verifyToken(token);
+    const { id, username } = decodedToken;
+    const CheckUser = prisma.admin.findUnique({ where: { id: id } });
+    if (!CheckUser) {
+      throw new CustomAPIError("Unauthorized", 401);
+    }
+    req.user = decodedToken; // Store user information in request object for later use
+    next(); // Move to the next middleware or route handler
+  } catch (error) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
+module.exports = { verifyTokenUser, verifyTokenAdmin };
