0ec7025d8a0da55d5eee826fe3133bd0fee5c4c9
nisa-basalamah <73590231+nisa-basalamah@users.noreply.github.com>
2023-09-16 23:58:16 +0700

    pulling development

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const crudProduct = require("./api/routes/product.route");
 const userRoute = require("./api/routes/user.route");
 
 const crudCity = require("./api/routes/city.route");
-const crudAddress = require("./api/routes/address.route");
+
 
 const adminRoute = require("./api/routes/cms/cms.admin.route");
 const crudCategory = require("./api/routes/cms/cms.categories.route");
@@ -14,7 +14,7 @@ const crudUserCMS = require("./api/routes/cms/cms.user.route");
 const crudCart = require("./api/routes/cart.route");
 const uploadFile = require("./lib/upload");
 const upload = require("./lib/multer");
-const crudAddressWeb = require("./api/routes/address.route");
+const crudAddress = require("./api/routes/address.route");
 const crudBank = require("./api/routes/bankAccount.route");
 const crudBankCMS = require("./api/routes/cms/cms.bankAccount.route");
 const {
@@ -31,13 +31,12 @@ router.use("/api/user", userRoute);
 router.use("/api/cart", crudCart);
 router.use("/api/address", crudAddress);
 router.use("/api/city", crudCity);
-router.use("/api", crudAddressWeb);
 router.use("/api/bank-accounts", crudBank);
 
 router.use(verifyTokenAdmin);
 router.use("/api/cms/category", crudCategory);
-router.use("/api/cms/subcategory", crudSubCategory); //BELUM DICEK
-router.use("/api/cms/products", crudProductCMS); //BELUM DICEK
+router.use("/api/cms/subcategory", crudSubCategory);
+router.use("/api/cms/products", crudProductCMS);
 router.use("/api/cms/users", crudUserCMS);
 router.use("/api/cms/bank-accounts", crudBankCMS);
 
