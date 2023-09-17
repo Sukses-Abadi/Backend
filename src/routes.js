65174e9f5442b736c6d14cfb987b142a6fc9447b
ling-ling30 <126201256+ling-ling30@users.noreply.github.com>
2023-09-17 17:52:08 +0700

    test pull

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,9 @@ const router = require("express").Router();
 
 const crudProduct = require("./api/routes/product.route");
 const userRoute = require("./api/routes/user.route");
-const reviewRoute = require("./api/routes/review.route"); 
+const reviewRoute = require("./api/routes/review.route");
 
 const crudCity = require("./api/routes/city.route");
-
 
 const adminRoute = require("./api/routes/cms/cms.admin.route");
 const crudCategory = require("./api/routes/cms/cms.categories.route");
@@ -18,15 +17,12 @@ const upload = require("./lib/multer");
 const crudAddress = require("./api/routes/address.route");
 const crudBank = require("./api/routes/bankAccount.route");
 const crudBankCMS = require("./api/routes/cms/cms.bankAccount.route");
-<<<<<<< HEAD
 const crudOrder = require("./api/routes/order.routes");
 const crudOrderCMS = require("./api/routes/cms/cms.order.route");
-=======
 const {
   verifyTokenAdmin,
 } = require("../src/api/middlewares/verifyTokenMiddleware");
 
->>>>>>> development
 /**
  * api routes
  */
@@ -40,21 +36,14 @@ router.use("/api/address", crudAddress);
 router.use("/api/city", crudCity);
 router.use("/api/bank-accounts", crudBank);
 router.use("/api/products", reviewRoute);
-
-<<<<<<< HEAD
 router.use("/api/order", crudOrder);
-router.use("/api/cms/bank-accounts", crudBankCMS);
-=======
+
 router.use(verifyTokenAdmin);
->>>>>>> development
+router.use("/api/cms/bank-accounts", crudBankCMS);
 router.use("/api/cms/category", crudCategory);
 router.use("/api/cms/subcategory", crudSubCategory);
 router.use("/api/cms/products", crudProductCMS);
 router.use("/api/cms/users", crudUserCMS);
-<<<<<<< HEAD
 router.use("/api/cms/order", crudOrderCMS);
-=======
-router.use("/api/cms/bank-accounts", crudBankCMS);
 
->>>>>>> development
 module.exports = router;
