0d587c227829ad6dbabeefadc868b7370f6132de
nisa-basalamah <73590231+nisa-basalamah@users.noreply.github.com>
2023-10-14 02:04:06 +0700

    change back the uploads folder path

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -1,18 +1,9 @@
 const multer = require("multer");
 const fs = require("fs");
 
-// const createUploadsFolder = () => {
-//   const folderPath = "./uploads"; // Specify the folder path
-
-//   if (!fs.existsSync(folderPath)) {
-//     fs.mkdirSync(folderPath);
-//     // console.log("Uploads folder created successfully.");
-//   }
-// };
-// createUploadsFolder();
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
