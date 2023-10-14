f2268b638fe4a47ba9bb5d122cbc48c5fc597c5d
nisa-basalamah <73590231+nisa-basalamah@users.noreply.github.com>
2023-10-14 21:06:03 +0700

    adjust product photo link

--- prisma/seed.js
+++ prisma/seed.js
@@ -604,67 +604,83 @@ async function main() {
   await prisma.productGallery.createMany({
     data: [
       {
-        photo: "Long-Sleeve-T-shirt-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Long-Sleeve-T-shirt-1.jpg?alt=media&token=08cf6582-96ac-48d2-8096-4ea0f1badc53&_gl=1*1tg41jv*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTM1Ni42LjAuMA..",
         product_id: 1,
       },
       {
-        photo: "Long-Sleeve-T-shirt-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Long-Sleeve-T-shirt-2.jpg?alt=media&token=8e452632-2020-4b32-8122-b0d1e8ce1a37&_gl=1*i5n6rg*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTM3MC42MC4wLjA.",
         product_id: 1,
       },
       {
-        photo: "Short-Sleeve-T-shirt-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Short-Sleeve-T-shirt-1.jpg?alt=media&token=8ad38e1b-158c-48b8-90d8-76c8e8d4dcd0&_gl=1*13wryaq*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTM4Ni40NC4wLjA.",
         product_id: 2,
       },
       {
-        photo: "Short-Sleeve-T-shirt-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Short-Sleeve-T-shirt-2.jpg?alt=media&token=3dc697da-8283-48a1-b4e9-16299383c17e&_gl=1*a579ry*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQwNi4yNC4wLjA.",
         product_id: 2,
       },
       {
-        photo: "Sweater-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Sweater-1.jpg?alt=media&token=bce127a1-b64e-4365-815d-5727583e26ee&_gl=1*fsnxq4*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQxNy4xMy4wLjA.",
         product_id: 3,
       },
       {
-        photo: "Sweater-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Sweater-2.jpg?alt=media&token=26f94c65-dbf0-4ec5-b35f-b90f096066a4&_gl=1*1xu9tdq*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQyNi40LjAuMA..",
         product_id: 3,
       },
       {
-        photo: "Jeans-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Jeans-1.jpg?alt=media&token=2911b0b9-a114-423b-878a-ec3af8bb38b1&_gl=1*i7sqo4*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQ0Mi42MC4wLjA.",
         product_id: 4,
       },
       {
-        photo: "Jeans-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Jeans-2.jpg?alt=media&token=a1cd76f6-6c64-4c75-82e1-0e3d90a2b46b&_gl=1*myuhne*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQ0OS41My4wLjA.",
         product_id: 4,
       },
       {
-        photo: "Sweatpants-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Sweatpants-1.jpg?alt=media&token=ed759e4a-7540-4711-a5a1-cb7689d65b15&_gl=1*tqyoer*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQ2Mi40MC4wLjA.",
         product_id: 5,
       },
       {
-        photo: "Sweatpants-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Sweatpants-2.jpg?alt=media&token=ae9e9304-212e-45d9-bec5-2964910d0b45&_gl=1*15daj2s*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQ3NC4yOC4wLjA.",
         product_id: 5,
       },
       {
-        photo: "Shorts-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Shorts-1.jpg?alt=media&token=d425cd72-5f18-477f-bb6b-3bae2e0f7415&_gl=1*14c3rye*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQ4NS4xNy4wLjA.",
         product_id: 6,
       },
       {
-        photo: "Shorts-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Shorts-2.jpg?alt=media&token=5558b23a-617f-41cf-b61e-5ec955aeb4b5&_gl=1*c065ts*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTQ5My45LjAuMA..",
         product_id: 6,
       },
       {
-        photo: "Jacket-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Jacket-1.jpg?alt=media&token=5ab58c9b-8d93-4736-9b4d-70ca54b1f7e3&_gl=1*wegvsq*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTUwOC42MC4wLjA.",
         product_id: 7,
       },
       {
-        photo: "Jacket-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Jacket-2.jpg?alt=media&token=865713c7-3471-4f41-895d-b6c398d6a85d&_gl=1*1i5xsi0*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTUyMy40NS4wLjA.",
         product_id: 7,
       },
       {
-        photo: "Hoodie-1.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Hoodie-1.jpg?alt=media&token=47c634c3-f1dc-47d3-ac1b-9dc95d29a597&_gl=1*gas7ku*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTUzMS4zNy4wLjA.",
         product_id: 8,
       },
       {
-        photo: "Hoodie-2.jpg",
+        photo:
+          "https://firebasestorage.googleapis.com/v0/b/sukses-abadi.appspot.com/o/Hoodie-2.jpg?alt=media&token=d4fe77c2-7ca7-425f-8824-d08963f0230b&_gl=1*k3j6mg*_ga*MTEwMTA5MjIyNS4xNjk3MjcwMjQ5*_ga_CW55HF8NVT*MTY5NzI5MTMwMi41LjEuMTY5NzI5MTUzOS4yOS4wLjA.",
         product_id: 8,
       },
     ],
