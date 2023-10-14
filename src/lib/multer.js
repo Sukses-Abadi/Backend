20dcf0e04613867526b2817ab3b77bde0b45d53f
ling-ling30 <126201256+ling-ling30@users.noreply.github.com>
2023-10-14 17:09:48 +0700

    add multer to firebase

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -1,22 +1,6 @@
 const multer = require("multer");
-const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname +
-        "-" +
-        uniqueSuffix +
-        "." +
-        file.originalname.split(".").pop()
-    );
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
